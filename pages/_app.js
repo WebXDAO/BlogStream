e4e9ed737e81cc3791c33ad629caf9898442f716
Eui Hyun Kim <hyomithetech@gmail.com>
2022-02-03 11:53:27 -0800

    Move layout component to the inner layer of app tfor other pages that don't need layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,10 @@
 import "../styles/globals.css";
-import Layout from "../components/global/Layout";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
+    <div>
       <Component {...pageProps} />
-    </Layout>
+    </div>
   );
 }
 
