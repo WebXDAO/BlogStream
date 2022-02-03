5ae668ae011c09090ee51897f8adf6aacc01fb40
Eui Hyun Kim <hyomithetech@gmail.com>
2022-02-03 10:49:55 -0800

    Create nextjs and tailwindcss app, and working on creating layout

---
+++ components/global/Layout.js
@@ -0,0 +1,14 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
