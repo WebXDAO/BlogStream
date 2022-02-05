e50776ca6a95f513b6e53b70e4bffb9230efea29
Eui Hyun Kim <hyomithetech@gmail.com>
2022-02-04 17:18:45 -0800

    Create walletsection

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,10 @@
-import Layout from '../components/global/Layout'
+import Layout from "../components/global/Layout";
+import WalletSection from "./walletsection";
 
 export default function Home() {
   return (
     <Layout>
-      <h1>hello world!</h1>
-
-      <h3>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at molestie nulla.
-        Pellentesque pretium eu orci eu interdum. Donec nunc turpis, feugiat id venenatis nec,
-        condimentum id erat. Aenean ut placerat velit. Fusce fermentum gravida dui id maximus.
-        Praesent pretium vestibulum condimentum. Aenean ac metus rhoncus, elementum nulla eget,
-        ultricies eros. Fusce rhoncus malesuada sodales. Curabitur egestas diam ultricies nibh
-        aliquam, in pellentesque ipsum rhoncus. Aenean tellus augue, accumsan at augue non, placerat
-        euismod nunc. Ut a metus ut risus mattis vehicula. In vel orci imperdiet, cursus augue eu,
-        rhoncus nunc. Quisque eget tortor purus. Nullam consequat eu magna at tristique. Nam
-        fringilla nisl sit amet urna aliquet pretium. Etiam lacinia lectus in suscipit tempus.
-        Curabitur nec nibh ligula. Phasellus purus mi, aliquet eu nibh a, hendrerit malesuada
-        ligula. Mauris dolor risus, congue id urna vel, congue eleifend ante. Suspendisse efficitur,
-        tortor ac ultricies euismod, nisl nulla ullamcorper arcu, in egestas augue mauris non erat.
-        Donec nisl dui, pharetra eget imperdiet a, scelerisque sit amet augue. Fusce est libero,
-        sollicitudin sit amet lacinia eget, viverra vitae erat. Sed dictum ligula eget ultricies
-        condimentum. Aliquam in tempus dui. Phasellus sodales sollicitudin lorem ac egestas. Mauris
-        porta justo sed pulvinar ultrices. Aliquam tempus elit ut dui tempor, accumsan mattis nisl
-        maximus. Aenean non felis molestie, sollicitudin velit ac, bibendum massa. Proin porttitor,
-        est vitae consectetur commodo, neque ex sagittis odio, a iaculis lorem felis nec nunc. Nulla
-        sapien turpis, gravida sed risus sed, pharetra cursus nunc. Phasellus enim nisi, fringilla
-        ac purus et, scelerisque interdum elit. Vivamus vitae justo sed velit tincidunt interdum.
-        Praesent at erat quis ante feugiat placerat. Duis vel tellus pulvinar, accumsan mauris quis,
-        placerat felis. Integer pellentesque volutpat leo.
-      </h3>
+      <WalletSection />
     </Layout>
-  )
+  );
 }
