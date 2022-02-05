502793aced412343377d99132ed6ad998f69a6a0
Abhinav MV <mvabhinav1998@gmail.com>
2022-02-05 19:16:14 +0530

    Save blog IPFS using Morallis

--- pages/createblog.js
+++ pages/createblog.js
@@ -16,6 +16,7 @@ function CreateBlog() {
   const [value, setValue] = useState('')
   const [title, setTitle] = useState('')
   const [imageFile, setImageFile] = useState(null)
+  const [fileURL, setFileUrl] = useState(null)
 
   const { isWeb3Enabled, enableWeb3, isAuthenticated, isWeb3EnableLoading, account } = useMoralis()
 
@@ -25,10 +26,16 @@ function CreateBlog() {
       enableWeb3({ provider: connectorId })
   }, [isAuthenticated, isWeb3Enabled])
 
-  function onChange(e) {
-    const file = e.target.files[0]
-    setImageFile(file)
-    imagecontainer.src = URL.createObjectURL(file)
+  async function onChange(e) {
+    try {
+      const f = e.target.files[0]
+      imagecontainer.src = URL.createObjectURL(f)
+      const file = new Moralis.File(f.name, f)
+      await file.saveIPFS()
+      setFileUrl(file.ipfs())
+    } catch (error) {
+      console.log('Image Upload Error', error)
+    }
     // try {
     //   const added = await client.add(file, {
     //     progress: (prog) => console.log(`received: ${prog}`)
@@ -57,23 +64,26 @@ function CreateBlog() {
     //   } catch (error) {
     //     console.log('Error uploading file: ', error)
     //   }
-    if (!value || !title || !imageFile) return
+    if (!value || !title || !fileURL) return
     setIsLoading(true)
-    const file = new Moralis.File(imageFile.name, imageFile)
-    await file.saveIPFS()
-    console.log(file.ipfs(), file.hash())
-    const imgURL = file.ipfs()
-    const metadata = {
-      title,
-      author: account,
-      value,
-      imgURL,
-      createdAt: new Date()
+    try {
+      const metadata = {
+        title,
+        author: account,
+        value,
+        imgURL: fileURL,
+        createdAt: new Date()
+      }
+      const blog = new Moralis.File('blog.json', { base64: btoa(JSON.stringify(metadata)) })
+      await blog.saveIPFS()
+      console.log('blog', blog.ipfs())
+    } catch (error) {
+      console.log('Blog Error', error)
     }
-    const blog = new Moralis.File('blog.json', { base64: btoa(JSON.stringify(metadata)) })
-    await blog.saveIPFS()
-    console.log('blog', blog.ipfs())
     setIsLoading(false)
+    setFileUrl(null)
+    setTitle('')
+    setValue('')
   }
   return !isAuthenticated ? (
     <div className='h-screen flex bg-gray-300'>
@@ -86,9 +96,9 @@ function CreateBlog() {
     <Layout>
       <div className='px-4'>
         <div className='my-2 border-2 border-dashed text-center w-fit mx-auto p-5 rounded-lg'>
-          {<img src='#' id='imagecontainer' className='w-full max-h-32' />}
+          <img src='#' id='imagecontainer' className='w-full max-h-32' alt='' />
           <label className='text-xl text-center' htmlFor='image'>
-            {imageFile ? 'Image Added' : 'Choose an Image'}
+            {imageFile ? 'Image Added!' : 'Choose an Image'}
           </label>
           <input className='hidden' onChange={onChange} id='image' type='file' />
         </div>
@@ -107,13 +117,17 @@ function CreateBlog() {
         </div>
         <MDEditor height={500} value={value} onChange={setValue} />
         <div className='w-full flex'>
-          <button
-            disabled={value === '' || title === ''}
-            className='mx-auto my-4 w-fit p-4 px-6 rounded-xl border text-white text-xl bg-gradient-to-r from-[#3B82F6] to-[#D946EF]'
-            onClick={createData}
-          >
-            {isLoading ? 'Loading' : 'Submit'}
-          </button>
+          {isLoading ? (
+            <div className='border-2 rounded-full w-24 h-24 mx-auto my-4 border-l border-fuchsia-400 animate-spin' />
+          ) : (
+            <button
+              disabled={value === '' || title === ''}
+              className='mx-auto my-4 w-fit p-4 px-6 rounded-xl border text-white text-xl bg-gradient-to-r from-[#3B82F6] to-[#D946EF]'
+              onClick={createData}
+            >
+              Submit
+            </button>
+          )}
         </div>
       </div>
     </Layout>
