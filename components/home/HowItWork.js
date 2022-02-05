92a0ffa09eb06304fc4899071dad303c61f5bdd1
Eui Hyun Kim <hyomithetech@gmail.com>
2022-02-05 14:41:27 -0800

    Add 'How it work' section in landing page

---
+++ components/home/HowItWork.js
@@ -0,0 +1,21 @@
+import LabTopSVG from "../../public/landingpage/laptop.svg";
+
+const HowItWork = () => {
+  return (
+    <div className="flex flex-col items-center xl:block w-full mx-auto py-16 px-40">
+      <h1 className="text-3xl pb-20 font-bold">How it work?</h1>
+      <div className="flex flex-col items-center xl:flex-row xl:justify-around">
+        <LabTopSVG />
+        <div className="flex w-1/3 pt-10 xl:pt-0 flex-col justify-center items-center">
+          <h2 className="text-xl mb-4 font-bold">Read Blogs</h2>
+          <p className="text-center">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HowItWork;
