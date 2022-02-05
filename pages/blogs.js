e158f9b6e12f54298c8c39f5d75bc1073f5577c1
Abhinav MV <mvabhinav1998@gmail.com>
2022-02-05 18:56:51 +0530

    Add morallis IPFS

--- pages/blogs.js
+++ pages/blogs.js
@@ -1,5 +1,5 @@
 // https://ipfs.infura.io/ipfs/QmVHQx8AwCqgeY4dahPvsgBUWVB4hxRAjZT1rJyBevntmz
-
+// "https://ipfs.moralis.io:2053/ipfs/QmbzzckrEpqMUxkWsG9Tm5SYWEyTtqdhMnLGWaR73hW8g6"
 import Layout from '../components/global/Layout'
 
 const Blogs = () => {
--- pages/createblog.js
+++ pages/createblog.js
@@ -2,18 +2,20 @@ import '@uiw/react-md-editor/markdown-editor.css'
 import '@uiw/react-markdown-preview/markdown.css'
 import dynamic from 'next/dynamic'
 import { useEffect, useState } from 'react'
-import { create as ipfsHttpClient } from 'ipfs-http-client'
+// import { create as ipfsHttpClient } from 'ipfs-http-client'
 import Layout from '../components/global/Layout'
+import Moralis from 'moralis'
 import { useMoralis } from 'react-moralis'
 import Account from '../components/Account'
 
-const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0')
+// const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0')
 const MDEditor = dynamic(() => import('@uiw/react-md-editor'), { ssr: false })
 
 function CreateBlog() {
-  const [fileUrl, setFileUrl] = useState(null)
+  const [isLoading, setIsLoading] = useState(null)
   const [value, setValue] = useState('')
   const [title, setTitle] = useState('')
+  const [imageFile, setImageFile] = useState(null)
 
   const { isWeb3Enabled, enableWeb3, isAuthenticated, isWeb3EnableLoading, account } = useMoralis()
 
@@ -23,36 +25,55 @@ function CreateBlog() {
       enableWeb3({ provider: connectorId })
   }, [isAuthenticated, isWeb3Enabled])
 
-  async function onChange(e) {
+  function onChange(e) {
     const file = e.target.files[0]
-    try {
-      const added = await client.add(file, {
-        progress: (prog) => console.log(`received: ${prog}`)
-      })
-      const url = `https://ipfs.infura.io/ipfs/${added.path}`
-      setFileUrl(url)
-    } catch (error) {
-      console.log('Error uploading file: ', error)
-    }
+    setImageFile(file)
+    imagecontainer.src = URL.createObjectURL(file)
+    // try {
+    //   const added = await client.add(file, {
+    //     progress: (prog) => console.log(`received: ${prog}`)
+    //   })
+    //   const url = `https://ipfs.infura.io/ipfs/${added.path}`
+    //   setFileUrl(url)
+    // } catch (error) {
+    //   console.log('Error uploading file: ', error)
+    // }
   }
   async function createData() {
-    if (!value || !fileUrl) return
-    /* first, upload to IPFS */
-    const data = JSON.stringify({
-      value,
+    //   if (!value || !fileUrl) return
+    //   /* first, upload to IPFS */
+    //   const data = JSON.stringify({
+    //     value,
+    //     title,
+    //     author: account,
+    //     createdAt: new Date(),
+    //     image: fileUrl
+    //   })
+    //   try {
+    //     const added = await client.add(data)
+    //     const url = `https://ipfs.infura.io/ipfs/${added.path}`
+    //     /* after file is uploaded to IPFS, pass the URL to save it on Polygon */
+    //     console.log(url)
+    //   } catch (error) {
+    //     console.log('Error uploading file: ', error)
+    //   }
+    if (!value || !title || !imageFile) return
+    setIsLoading(true)
+    const file = new Moralis.File(imageFile.name, imageFile)
+    await file.saveIPFS()
+    console.log(file.ipfs(), file.hash())
+    const imgURL = file.ipfs()
+    const metadata = {
       title,
       author: account,
-      createdAt: new Date(),
-      image: fileUrl
-    })
-    try {
-      const added = await client.add(data)
-      const url = `https://ipfs.infura.io/ipfs/${added.path}`
-      /* after file is uploaded to IPFS, pass the URL to save it on Polygon */
-      console.log(url)
-    } catch (error) {
-      console.log('Error uploading file: ', error)
+      value,
+      imgURL,
+      createdAt: new Date()
     }
+    const blog = new Moralis.File('blog.json', { base64: btoa(JSON.stringify(metadata)) })
+    await blog.saveIPFS()
+    console.log('blog', blog.ipfs())
+    setIsLoading(false)
   }
   return !isAuthenticated ? (
     <div className='h-screen flex bg-gray-300'>
@@ -64,18 +85,19 @@ function CreateBlog() {
   ) : (
     <Layout>
       <div className='px-4'>
-        <div className='my-2 border-2 border-dashed w-fit mx-auto p-5 rounded-lg'>
-          <label className='text-2xl' htmlFor='image'>
-            {fileUrl ? 'Image Added' : 'Choose an Image'}
+        <div className='my-2 border-2 border-dashed text-center w-fit mx-auto p-5 rounded-lg'>
+          {<img src='#' id='imagecontainer' className='w-full max-h-32' />}
+          <label className='text-xl text-center' htmlFor='image'>
+            {imageFile ? 'Image Added' : 'Choose an Image'}
           </label>
           <input className='hidden' onChange={onChange} id='image' type='file' />
         </div>
-        <div class='mb-4 flex justify-center items-center space-x-4 mx-8'>
-          <label class='block text-gray-700 text-2xl font-bold mb-2' for='title'>
+        <div className='mb-4 flex justify-center items-center space-x-4 mx-8'>
+          <label className='block text-gray-700 text-2xl font-bold mb-2' htmlFor='title'>
             Title
           </label>
           <input
-            class='shadow appearance-none border rounded w-full py-4 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+            className='shadow appearance-none border rounded w-full py-4 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
             id='title'
             type='text'
             value={title}
@@ -90,7 +112,7 @@ function CreateBlog() {
             className='mx-auto my-4 w-fit p-4 px-6 rounded-xl border text-white text-xl bg-gradient-to-r from-[#3B82F6] to-[#D946EF]'
             onClick={createData}
           >
-            Submit
+            {isLoading ? 'Loading' : 'Submit'}
           </button>
         </div>
       </div>
