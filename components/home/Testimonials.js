6d06eba8c8f9c220ca0f3db5df8f9f0f8f32c447
Eui Hyun Kim <hyomithetech@gmail.com>
2022-02-05 22:58:10 -0800

    Working on testimonial section

---
+++ components/home/Testimonials.js
@@ -0,0 +1,25 @@
+// import SupportWithCrypto from "../public/landingpage/supportwithcrypto.svg";
+
+const Testimonials = () => {
+  return (
+    <div className="px-20">
+      <svg
+        width="170"
+        height="160"
+        viewBox="0 0 170 160"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M141.742 147.399C159.291 145.245 171.735 128.897 169.523 110.884C167.313 92.8815 151.283 80.0189 133.733 82.1737C101.958 86.0751 115.397 20.434 157.778 15.2303L155.91 0.0109468C80.2702 9.28725 68.2512 156.423 141.742 147.399V147.399ZM50.1783 158.642C67.7171 156.488 80.1604 140.141 77.9486 122.128C75.7382 104.125 59.7084 91.2628 42.1696 93.4163C10.384 97.319 23.8231 31.6779 66.2039 26.4742L64.3352 11.2549C-11.2932 20.5298 -23.3123 167.665 50.1783 158.642V158.642Z"
+          fill="#C767E5"
+        />
+      </svg>
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-pink-400 font-bold text-lg">TESTIMONIALS</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonials;
