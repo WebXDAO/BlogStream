12aa9e2af5933139a8ca6aa5823abd05d5cb1988
Abhinav MV <mvabhinav1998@gmail.com>
2022-02-06 19:54:19 +0530

    Typo fixed

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,23 @@
-import { useEffect } from "react";
-import { useMoralis } from "react-moralis";
-import Layout from "../components/global/Layout";
-import BottomCard from "../components/home/BottomCard";
-import HowItWork from "../components/home/HowItWork";
-import WalletSection from "../components/home/WalletSection";
+import { useEffect } from 'react'
+import { useMoralis } from 'react-moralis'
+import Layout from '../components/global/Layout'
+import BottomCard from '../components/home/BottomCard'
+import HowItWork from '../components/home/HowItWork'
+import WalletSection from '../components/home/WalletSection'
 
-import Hero from "../components/home/Hero";
-import LatestArticles from "../components/home/LatestArticles";
-import FAQ from "../components/home/FAQ";
-import Testimonials from "../components/home/testimonials";
+import Hero from '../components/home/Hero'
+import LatestArticles from '../components/home/LatestArticles'
+import FAQ from '../components/home/FAQ'
+import Testimonials from '../components/home/Testimonials'
 
 export default function Home() {
-  const { isWeb3Enabled, enableWeb3, isAuthenticated, isWeb3EnableLoading } =
-    useMoralis();
+  const { isWeb3Enabled, enableWeb3, isAuthenticated, isWeb3EnableLoading } = useMoralis()
 
   useEffect(() => {
-    const connectorId = window.localStorage.getItem("connectorId");
+    const connectorId = window.localStorage.getItem('connectorId')
     if (isAuthenticated && !isWeb3Enabled && !isWeb3EnableLoading)
-      enableWeb3({ provider: connectorId });
-  }, [isAuthenticated, isWeb3Enabled]);
+      enableWeb3({ provider: connectorId })
+  }, [isAuthenticated, isWeb3Enabled])
 
   return (
     // inside of layout
@@ -31,5 +30,5 @@ export default function Home() {
       <Testimonials />
       <BottomCard />
     </Layout>
-  );
+  )
 }
