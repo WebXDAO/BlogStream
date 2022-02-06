29ab1868653c34f7d42352f04e264300e46c1bcf
Eui Hyun Kim <hyomithetech@gmail.com>
2022-02-05 22:57:10 -0800

    Add latest article section into landingpage and created image carousel

---
+++ components/home/LatestArticles.js
@@ -0,0 +1,79 @@
+import Carousel from "../global/Carousel";
+
+const articleData = [
+  {
+    id: 1,
+    subject:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+    author: "Sreekar Sarvepalli",
+    pfpSrc: "/landingpage/hottestguy.png",
+    position: "Blockchain Specialist",
+  },
+  {
+    id: 2,
+    subject:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+    author: "Sreekar Sarvepalli",
+    pfpSrc: "/landingpage/hottestguy.png",
+    position: "Blockchain Specialist",
+  },
+  {
+    id: 3,
+    subject:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+    author: "Sreekar Sarvepalli",
+    pfpSrc: "/landingpage/hottestguy.png",
+    position: "Blockchain Specialist",
+  },
+  {
+    id: 4,
+    subject:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+    author: "Sreekar Sarvepalli",
+    pfpSrc: "/landingpage/hottestguy.png",
+    position: "Blockchain Specialist",
+  },
+  {
+    id: 5,
+    subject:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+    author: "Sreekar Sarvepalli",
+    pfpSrc: "/landingpage/hottestguy.png",
+    position: "Blockchain Specialist",
+  },
+  {
+    id: 6,
+    subject:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+    author: "Sreekar Sarvepalli",
+    pfpSrc: "/landingpage/hottestguy.png",
+    position: "Blockchain Specialist",
+  },
+  {
+    id: 7,
+    subject:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+    author: "Sreekar Sarvepalli",
+    pfpSrc: "/landingpage/hottestguy.png",
+    position: "Blockchain Specialist",
+  },
+  {
+    id: 8,
+    subject:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+    author: "Sreekar Sarvepalli",
+    pfpSrc: "/landingpage/hottestguy.png",
+    position: "Blockchain Specialist",
+  },
+];
+
+const LatestArticles = () => {
+  return (
+    <div className="h-[65vh]">
+      <h1 className="text-3xl px-40 pb-20 font-bold">Latest Articles</h1>
+      <Carousel data={articleData} />
+    </div>
+  );
+};
+
+export default LatestArticles;
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,31 @@
-import { useEffect } from 'react'
-import { useMoralis } from 'react-moralis'
-import Layout from '../components/global/Layout'
-import BottomCard from '../components/home/BottomCard'
+import { useEffect } from "react";
+import { useMoralis } from "react-moralis";
+import Layout from "../components/global/Layout";
+import BottomCard from "../components/home/BottomCard";
 
-import Hero from '../components/home/Hero'
+import Hero from "../components/home/Hero";
+import LatestArticles from "../components/home/LatestArticles";
+import FAQ from "../components/home/FAQ";
+import Testimonials from "../components/home/testimonials";
 
 export default function Home() {
-  const { isWeb3Enabled, enableWeb3, isAuthenticated, isWeb3EnableLoading } = useMoralis()
+  const { isWeb3Enabled, enableWeb3, isAuthenticated, isWeb3EnableLoading } =
+    useMoralis();
 
   useEffect(() => {
-    const connectorId = window.localStorage.getItem('connectorId')
+    const connectorId = window.localStorage.getItem("connectorId");
     if (isAuthenticated && !isWeb3Enabled && !isWeb3EnableLoading)
-      enableWeb3({ provider: connectorId })
-  }, [isAuthenticated, isWeb3Enabled])
+      enableWeb3({ provider: connectorId });
+  }, [isAuthenticated, isWeb3Enabled]);
 
   return (
     // inside of layout
     <Layout>
       <Hero />
+      <LatestArticles />
+      <FAQ />
+      <Testimonials />
       <BottomCard />
     </Layout>
-  )
+  );
 }
