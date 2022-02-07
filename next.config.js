f8414b78c269ca3ef87857075c8de6b25e97bb4e
Eui Hyun Kim <hyomithetech@gmail.com>
2022-02-06 17:51:23 -0800

    Add exporting object for deployment on fleek.co

--- next.config.js
+++ next.config.js
@@ -1,15 +1,19 @@
-const removeImports = require('next-remove-imports')()
+const removeImports = require("next-remove-imports")();
 module.exports = removeImports({
   reactStrictMode: true,
   images: {
-    domains: ['ipfs.moralis.io']
+    domains: ["ipfs.moralis.io"],
   },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack']
-    })
-    return config
-  }
-})
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+});
+
+module.exports = {
+  exportTrailingSlash: true,
+};
