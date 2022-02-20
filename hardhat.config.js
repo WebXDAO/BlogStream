b88905f91272c703e1bd949a54c538292ffa2b63
Abhinav MV <mvabhinav1998@gmail.com>
2022-02-20 11:45:32 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,12 +6,12 @@ module.exports = {
   networks: {
     hardhat: {
       chainId: 1337
-    },
-    //these will be used for deployment later on
-    mumbai: {
-      url: 'https://rpc-mumbai.matic.today',
-      accounts: [process.env.pk]
     }
+    //these will be used for deployment later on
+    // mumbai: {
+    //   url: 'https://rpc-mumbai.matic.today',
+    //   accounts: [process.env.pk]
+    // }
     // polygon: {
     //   url: "https://polygon-rpc.com/",
     //   accounts: [process.env.pk]
